0f07168d4b6464a25941e91c55ca23f6b1b14eb2
Anil Kumar <anilkumarnayak648@gmail.com>
2024-11-21 20:52:52 +0530

    adding new programs

--- PACKAGES/MyMath.java
+++ PACKAGES/MyMath.java
@@ -2,13 +2,13 @@ package btech.arithmatic;
 
 public class MyMath{
     public void add(int a, int b){
-        System.err.println("addition is : "+ (a+b));
+        System.out.println("addition is : "+ (a+b));
     }
     public void sub(int a, int b){
         System.out.println("substraction is : "+ (a-b));
     }
     public void mult(int a, int b){
-        System.err.println("multiplication is : "+ (a*b));
+        System.out.println("multiplication is : "+ (a*b));
     }
     public void div(int a, int b){
         System.out.println("division is : "+ (a/b));
